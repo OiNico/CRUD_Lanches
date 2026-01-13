c989c58a67e2764c326284cce6f448d158c5f917
Nicolas Gomes <soneca144p@gmail.com>
2026-01-13 17:55:46 -0300

    Criação da tabela de produtos e de categorias, alteração do nome da categoria compras para vendas

--- main.py
+++ main.py
@@ -4,15 +4,31 @@ import sqlite3
 #Conecta com o banco e instanciona o Cursor
 conection = sqlite3.connect("banco.db")
 cursor = conection.cursor()
+
+#Cria a tabela de produtos
+cursor.execute("""CREATE TABLE IF NOT EXISTS produtos(
+               Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+               Descricao TEXT NOT NULL,
+               Preco FLOAT NOT NULL,
+               IdCategoria INTEGER,
+               Imagem BLOB
+               )""")
+
+#Cria a tabela de categorias dos produtos
+cursor.execute("""CREATE TABLE IF NOT EXISTS categorias(
+               Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+               Descricao TEXT NOT NULL
+               )""")
+
 #Cria a tabela para registrar as compras                      cliente -> funcionário
-cursor.execute("""CREATE TABLE IF NOT EXISTS compras(
+cursor.execute("""CREATE TABLE IF NOT EXISTS vendas(
                Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
                Data TEXT NOT NULL,
                Cliente TEXT NOT NULL,
-               ValorDaCompra FLOAT NOT NULL
+               ValorDaVenda FLOAT NOT NULL
                )""")
 conection.commit()
-
+conection.close()
 
 #criação e configuração da telaPrincipal
 telaInicial = Tk()
