cfe2b95134721997ee7afcb32399a8f7895064b8
Nicolas Gomes <soneca144p@gmail.com>
2026-01-13 17:26:52 -0300

    Criação da Tabela de compras no banco de dados

--- main.py
+++ main.py
@@ -3,12 +3,21 @@ import sqlite3
 
 #Conecta com o banco e instanciona o Cursor
 conection = sqlite3.connect("banco.db")
-cursor = conection.Cursor()
+cursor = conection.cursor()
+#Cria a tabela para registrar as compras                      cliente -> funcionário
+cursor.execute("""CREATE TABLE IF NOT EXISTS compras(
+               Id INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,
+               Data TEXT NOT NULL,
+               Cliente TEXT NOT NULL,
+               ValorDaCompra FLOAT NOT NULL
+               )""")
+conection.commit()
+
 
 #criação e configuração da telaPrincipal
-telaPrincipal = Tk()
-telaPrincipal.title("Crud")
-telaPrincipal.config(height=616,width=1066)
+telaInicial = Tk()
+telaInicial.title("Crud")
+telaInicial.config(height=616,width=1066)
 
 
-telaPrincipal.mainloop()
+telaInicial.mainloop()
