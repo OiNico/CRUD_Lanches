61773b9d4a4a683bec68e48a95b3fe8069e2d59a
Nicolas Gomes <soneca144p@gmail.com>
2026-01-15 12:57:01 -0300

    Configuração do grid para ocupar toda a tela, criação do botão para voltar da tela de cadastro para a tela inicial

--- main.py
+++ main.py
@@ -39,15 +39,31 @@ Janela.title("Crud Lanches")
 telaInicial = tk.Frame(Janela)
 telaInicial.pack(fill="both", expand= True)
 
+# Faz o grid ocupar todo o frame - by ChatGPT
+telaInicial.columnconfigure(1, weight=1)
+telaInicial.rowconfigure(0, weight=1)
+telaInicial.rowconfigure(1, weight=1)
+telaInicial.rowconfigure(2, weight=1)
+
 def IrTelaCadastro():
     telaInicial.pack_forget()
-    TelaCadastro.pack()
+    TelaCadastro.pack(fill="both", expand=True)
+def IrTelaInicial():
+    TelaCadastro.pack_forget()
+    telaInicial.pack(fill="both",expand=True)
 
-tk.Label(telaInicial, text="Bem vindo ao Sistema de Lanches").pack()
-tk.Button(telaInicial, text="Cadastrar", command=IrTelaCadastro).pack()
+tk.Label(telaInicial, text="Bem vindo ao Sistema de Lanches").grid(row=0,column=0,padx=5,pady=5, sticky="n")
+tk.Button(telaInicial, text="Cadastrar", command=IrTelaCadastro).grid(row=10,column=10,padx=10, pady=10, sticky="e")
 
 #configuração da tela de cadastro
 TelaCadastro = tk.Frame()
-tk.Label(TelaCadastro, text="Tela de cadastro").pack()
+tk.Label(TelaCadastro, text="Tela de cadastro").grid(row=0,column=0,padx=5,pady=5, sticky="n")
+tk.Button(TelaCadastro, text="Voltar", command=IrTelaInicial).grid(row=10,column=10,padx=10, pady=10, sticky="e")
+
+# Faz o grid ocupar todo o frame - by ChatGPT
+TelaCadastro.columnconfigure(1, weight=1)
+TelaCadastro.rowconfigure(0, weight=1)
+TelaCadastro.rowconfigure(1, weight=1)
+TelaCadastro.rowconfigure(2, weight=1)
 
 Janela.mainloop()
